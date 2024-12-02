ce15ffdc574e1886b93a49d947c23594a442b0f7
Pete Doswell <pete.doswell@gmail.com>
2024-12-02 09:05:29 +0000

    data lines converted to lists of integers

--- day_2.py
+++ day_2.py
@@ -1,7 +1,19 @@
 # part 1 
 
 with open('day2_input.txt', 'r') as file:
-    input = file.read().splitlines()
+    data = file.read().splitlines()
 
-for line in input:
-    print(line)
+# convert each line to list of integers
+for line in data:
+    data_list = list(map(int, line.split()))
+
+
+
+
+
+
+#notes: lines are diff lengths, tf need dynamic (range(len)) process
+#TODO: define increasing or decreasing for first 2 numbers (subtraction)
+#TODO: iterate through each number in list, determining diff size and increase /decrease consistency
+#TODO: incerase counter/bucket list for each line that satisfies conds
+    
