5303b09c463a190289e405fea6878f72e5b2b3b9
Pete Doswell <pete.doswell@gmail.com>
2024-12-02 13:55:30 +0000

    part 1 solution

--- day_2.py
+++ day_2.py
@@ -9,26 +9,27 @@ data_list = [list(map(int, line.split())) for line in data]
 safe_count = 0
 
 for dat in data_list:
+
     l = len(dat) - 1
     increase = False
     decrease = False
     safety = True
 
-    for n in dat:
+    for n in range(len(dat)-1):
         # gap too large or no increase/decrease = unsafe
-        if abs(n - (n+1)) > 3 or n - (n+1) == 0:
+        if abs(dat[n] - dat[n+1]) > 3 or (dat[n] - (dat[n+1])) == 0:
             safety = False
             break
         # increasing
-        elif n - (n+1) > 0:
+        elif (dat[n] - dat[n+1]) > 0:
             increase = True
         # decreasing
-        elif n - (n+1) < 0:
+        elif (dat[n] - dat[n+1]) < 0:
             decrease = True
 
-    # fluctuation = unsafe
     if not safety: 
         continue
+    # fluctuation = unsafe
     if increase and decrease:
         continue
     else:
@@ -40,10 +41,4 @@ print(safe_count)
 
 
 
-
-#notes: lines are diff lengths, tf need dynamic (range(len)) process
-# note 2: sometimes consecutive numbers don't ascend or descend. So may need to create initial flag, and then check agains this as nunmbers are iterated over
-#TODO: define increasing or decreasing for first 2 numbers (subtraction)
-#TODO: iterate through each number in list, determining diff size and increase /decrease consistency
-#TODO: incerase counter/bucket list for each line that satisfies conds
     
