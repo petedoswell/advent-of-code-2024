f3fd99d60bbad372828d8df43d8d30fd703325d6
Pete Doswell <pete.doswell@gmail.com>
2024-12-03 07:27:23 +0000

    final solution part 1

--- day_3.py
+++ day_3.py
@@ -1,21 +1,26 @@
 import re
 
-#TODO: read file as continuous line
+# part 1
+
+#read file as continuous line
 with open('input-2.txt', 'r') as file:
 	data = file.read()
 
-#TODO: regex match mul(\d+,\d+) to list
+#regex match mul(\d+,\d+) to list
 pattern = r'mul\(\d+[,]\d+\)'
 matches = re.findall(pattern, data)
 
-#TODO: remove mul 
+#remove mul 
 trimmed = [match.replace('mul', '') for match in matches]
 
-#TODO: convert string to numerical tuples
+#convert string to numerical tuples
 trimmed = [tuple(map(int, trim.strip('()').split(','))) for trim in trimmed]
-print(trimmed)
+
+#multiply tuples & sum results
+mul = [a*b for a,b in trimmed]
+print(sum(mul))
+	
 
 
-#TODO: multiply tuples
-#TODO: sum results
+
 
