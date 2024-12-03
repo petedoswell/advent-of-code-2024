c0cdb1e642b0e19b3c221149a45893f6b612e430
Pete Doswell <pete.doswell@gmail.com>
2024-12-03 07:15:19 +0000

    read file, scrape regex

--- day_3.py
+++ day_3.py
@@ -1,1 +1,19 @@
-#placeholder for day 3
+import re
+
+#TODO: read file as continuous line
+with open('input 2.txt', 'r') as file:
+	data = file.read()
+
+#TODO: regex match mul(\d+,\d+) to list
+pattern = r'mul\(\d+[,]\d+\)'
+matches = re.findall(pattern, data)
+
+#TODO: remove mul 
+trimmed = [match.replace('mul', '') for match in matches]
+print(trimmed)
+
+#TODO: convert string to numerical tuples
+
+#TODO: multiply tuples
+#TODO: sum results
+
