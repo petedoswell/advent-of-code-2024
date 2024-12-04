2a1731f85a7351ab93b7a0b2d193c408b39710f4
Pete Doswell <pete.doswell@gmail.com>
2024-12-04 17:47:08 +0000

    loaded data into numpy array

--- day_4.py
+++ day_4.py
@@ -1,1 +1,6 @@
-day 4 placeholder
+import numpy as np 
+
+# load data into np array
+with open('input_day4.txt', 'r') as file:
+    data_list = [list(line) for line in file.read().splitlines()]  
+xmas_array = np.array(data_list)
