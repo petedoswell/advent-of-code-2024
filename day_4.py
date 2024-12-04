b634868584811d98ad713d02b96a3cebc834f56c
Pete Doswell <pete.doswell@gmail.com>
2024-12-04 19:12:43 +0000

    added array iterator and basic directional slices

--- day_4.py
+++ day_4.py
@@ -3,4 +3,12 @@ import numpy as np
 # load data into np array
 with open('input_day4.txt', 'r') as file:
     data_list = [list(line) for line in file.read().splitlines()]  
-xmas_array = np.array(data_list)
+arr = np.array(data_list)
+
+rows, cols = arr.shape
+
+for x in range(rows):
+    for y in range(cols):
+        forward_slice = arr[x, y:]
+
+        print(forward_slice)
