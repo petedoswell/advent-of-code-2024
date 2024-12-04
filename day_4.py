dd61e67bbf80112f461975de7c01a71ff3fc83be
Pete Doswell <pete.doswell@gmail.com>
2024-12-04 20:25:57 +0000

    iteration and basic directional search

--- day_4.py
+++ day_4.py
@@ -7,8 +7,31 @@ arr = np.array(data_list)
 
 rows, cols = arr.shape
 
+number_xmas = 0
+
+def xmas_counter(slice):
+    global number_xmas
+    if ''.join(slice) == 'XMAS':
+                number_xmas +=1
+
+
 for x in range(rows):
     for y in range(cols):
-        forward_slice = arr[x, y:]
 
-        print(forward_slice)
+        if y + 4 <= cols:
+            forward_slice = arr[x, y:(y+4)]
+            xmas_counter(forward_slice)
+            
+        if y - 3 >= 0:
+            backward_slice = arr[x,y:(y-4):-1]
+            xmas_counter(backward_slice)
+
+        if x + 4 <= rows:
+            downward_slice = arr[x:(x+4),y]
+            xmas_counter(downward_slice)
+        
+        if x - 3 >= 0:
+            upward_slice = arr[x:(x-4):-1,y]
+            xmas_counter(upward_slice)
+
+print(number_xmas)
