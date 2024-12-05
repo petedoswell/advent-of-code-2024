6478379f31f53eccc5ab97765d7a6d177d75adf7
Pete Doswell <pete.doswell@gmail.com>
2024-12-05 22:20:06 +0000

    data loaded into lists and tuples

--- day_5.py
+++ day_5.py
@@ -1,5 +1,10 @@
-#TODO: load data(1) into dicts (use regex?)
-#TODO: load data(2) into lists
+with open('day5_input.txt', 'r') as file:
+    data = file.read().splitlines()
+
+pg_order = [tuple(dat.split(r'|')) for dat in data if r'|' in dat]
+prnt_pgs = [dat for dat in data if r'|' not in dat]
+
+
 #TODO: iterate through data(2) using enumerate 
 #TODO: check dict v position < | > dict k position 
 #TODO: move data(2) list to valid | invalid list
