f6ba3ef964310a4e0f35a3382baab80580fccf07
Pete Doswell <pete.doswell@gmail.com>
2024-12-05 07:38:53 +0000

    to do list

--- day_5.py
+++ day_5.py
@@ -1,1 +1,8 @@
-# day 5 placeholder
+#TODO: load data(1) into dicts (use regex?)
+#TODO: load data(2) into lists
+#TODO: iterate through data(2) using enumerate 
+#TODO: check dict v position < | > dict k position 
+#TODO: move data(2) list to valid | invalid list
+#TODO: get middle number from each valid list (enumerate modulo?)
+#TODO: sum middle numbers
+
