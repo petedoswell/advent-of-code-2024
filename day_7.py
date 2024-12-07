d3709d214e4a1e4f650c9e47402007b9e7beabaf
Pete Doswell <pete.doswell@gmail.com>
2024-12-07 13:20:30 +0000

    part 2 solution

--- day_7.py
+++ day_7.py
@@ -1,3 +1,13 @@
+import logging
+
+# set up logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='optree_debug.log',
+    filemode='w'  
+)
+
 with open('day7_input.txt', 'r') as file:
 	data = [line.split() for line in file.read().splitlines()]
 	
@@ -10,6 +20,8 @@ valid_list = []
 
 for dat in data:
     ref_number = dat[0]
+    logging.info(f'ref number: {ref_number}')
+    logging.info(f'parameter numbers: {dat[1:]}')
     
     # insert top level for loop
     previous_level = [dat[1]]
@@ -17,10 +29,16 @@ for dat in data:
     for i in range(2, len(dat)):
 
         new_level_addition = [dat[i] + p for p in previous_level]
+        logging.info(f'addition list: {new_level_addition}')
         new_level_multiplication = [dat[i] * p for p in previous_level]
+        logging.info(f'multiplication list: {new_level_multiplication}')
+        new_level_concat = [int((str(p)) + str(dat[i])) for p in previous_level]
+        logging.info(f'concat list: {new_level_concat}')
 
-        new_level = zip(new_level_addition, new_level_multiplication)
+        new_level = zip(new_level_addition, new_level_multiplication, new_level_concat)
         flatten_new_level = [num for tp in new_level for num in tp ]
+        logging.info(f'flattened list: {flatten_new_level}')
+        
 
         previous_level = flatten_new_level
 
