b76aa259729a58ba4629b9010a35697eafe0141d
Pete Doswell <pete.doswell@gmail.com>
2024-12-07 17:17:18 +0000

    movement and turning algorithms added

--- day_6.py
+++ day_6.py
@@ -1,55 +1,31 @@
 import numpy as np 
 
-with open('day6_input.txt', 'r') as file:
+with open('day6_example.txt', 'r') as file:
     data = [list(line) for line in file.read().splitlines()]
 
 grid = np.array(data)
+print(grid)
 
 
-""" 
-^ = up
-< = left
-> = right
-v = down
-# = obstacle
-. = clear path
-X = patrol route
-
-
-to determine: 
-- position of guard
-- guard direction (i.e. symbol)
-- path clear or obstacle
-
-to make:
-turning algorithm
-moving algorithm
-
-if . or X then move 
-if # then turn right
-"""
-grid = 1
 guard_coords = (0,0)
 guard_dir = ''
 
-def guard_move(guard_dir, x,y):
-    """ define next step for guard """
-    if guard_dir == '^':
-        step = (x, y+1)
-    elif guard_dir == 'v':
-        step = (x+1, y)
-    elif guard_dir == '<':
-        step = (y-1)
-    elif guard_dir == '>':
-        step = (x, y+1)
-    return step
 
-def check_path(x,y):
-    """ checks next step for obstacles """
-    if (x,y) == '.' or (x,y) == 'X':
-        return 'move'
-    elif (x,y) == '#':
-        return 'turn right'
+def slice(guard_dir,guard_coords):
+    """ returns np slice from guard to edge of array """
+    if guard_dir == '^':
+        upward_slice = grid[guard_coords[0]::-1,guard_coords[1]]
+        return  upward_slice
+    elif guard_dir == '>':
+        right_slice = grid[guard_coords[0],guard_coords[1]:]
+        return right_slice
+    elif guard_dir == 'v':
+        down_slice = grid[guard_coords[0]:,guard_coords[1]]
+        return down_slice
+    elif guard_dir == '<':
+        left_slice = grid[guard_coords[0],guard_coords[1]::-1]
+        return left_slice
+
 
 def guard_turn(guard_dir):
     """ turns guard right """
@@ -62,11 +38,4 @@ def guard_turn(guard_dir):
     elif guard_dir == '<':
         return '^'
 
-if check_path == 'move':
-    #grid(guard_coords) = 'X'
-    #grid(guard_coords = guard_dir
-    pass
-         
-elif check_path == 'turn right':
-    guard_turn(guard_dir)
-    guard_move()
+
