9a43bcb4376ddf8c6b018fb1c877c8af8534b789
Pete Doswell <pete.doswell@gmail.com>
2024-12-07 13:02:13 +0000

    part 1 solution complete

--- day_7.py
+++ day_7.py
@@ -1,6 +1,4 @@
-import binarytree
-
-with open('day7_example.txt', 'r') as file:
+with open('day7_input.txt', 'r') as file:
 	data = [line.split() for line in file.read().splitlines()]
 	
 for dat in data:
@@ -11,13 +9,10 @@ data = [list(map(int, dat)) for dat in data]
 valid_list = []
 
 for dat in data:
-    print('dat:' , dat)
     ref_number = dat[0]
-    print('ref_number: ', ref_number)
     
     # insert top level for loop
     previous_level = [dat[1]]
-    print('previous level: ', previous_level)
 
     for i in range(2, len(dat)):
 
@@ -26,11 +21,10 @@ for dat in data:
 
         new_level = zip(new_level_addition, new_level_multiplication)
         flatten_new_level = [num for tp in new_level for num in tp ]
-        print('flattened list: ', flatten_new_level)
 
         previous_level = flatten_new_level
 
     if ref_number in previous_level:
         valid_list.append(ref_number)
 
-print(valid_list)
+print(sum(valid_list))
