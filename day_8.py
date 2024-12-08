2ae6fc32edf7cf567e0b4445924538e5726853cf
Pete Doswell <pete.doswell@gmail.com>
2024-12-08 12:11:36 +0000

    get coords of antennae

--- day_8.py
+++ day_8.py
@@ -4,10 +4,21 @@ with open('day8_example.txt', 'r') as file:
     data = [list(line) for line in file.read().splitlines()]
 
 grid = np.array(data)
-print(grid)
 
-#TODO: define parameters of grid
-#TODO: scrape coords of antenna groups
+# define grid parameters, set vars to ref axes
+rows, cols = grid.shape
+
+# get list of antenna chars
+antennae = np.unique(grid).tolist()
+antennae.remove('.')
+print(f'\nantennae in grid: {antennae}\n')
+
+for ant in antennae:
+    # get indices of element and convert to list
+    coords = np.argwhere(grid == ant).tolist()
+    print(f'{ant} coords: {coords}')
+
+
 #TODO: iterate through group cooords, return antinode coords, save to new list
 #TODO: remove out-of-bounds antinode coords.
 #TODO: count antinode coords (do overlapping antinodes count as 1 or > 1?)
