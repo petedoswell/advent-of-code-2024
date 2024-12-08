539e499acc59102e3a567d3e76b8f16635be302a
Pete Doswell <pete.doswell@gmail.com>
2024-12-08 16:02:12 +0000

    added functions to determine distance between two antennae and position of corresponding anitnodes

--- day_8.py
+++ day_8.py
@@ -19,6 +19,26 @@ for ant in antennae:
     print(f'{ant} coords: {coords}')
 
 
+
+def antinode_coords(coords):
+    for i in range(len(coords)-1):
+        # section off ref antenna
+        antenna_1 = coords.pop(0)
+        # ref antenna coords
+        for a1_x,a1_y in antenna_1:
+            # comparison antenna coords
+            for a2_x,a2_y in coords:
+                # calc dist between ref antenna and comparision antenna
+                x_dist = max([a1_x, a2_x]) - min([a1_x], [a2_x])
+                y_dist = max([a1_y, a2_y]) - min([a1_y], [a2_y])
+                antinode_1 = 
+
+
+antinode_coords(coords)
+
+def antenna_distance(ant_1, ant_2):
+    
+
 #TODO: iterate through group cooords, return antinode coords, save to new list
 #TODO: remove out-of-bounds antinode coords.
 #TODO: count antinode coords (do overlapping antinodes count as 1 or > 1?)
