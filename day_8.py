bec036a8a05f7a6f229f60b85a10a21b9c92f33c
Pete Doswell <pete.doswell@gmail.com>
2024-12-08 16:02:37 +0000

    added functions to determine distance between two antennae and position of corresponding anitnodes

--- day_8.py
+++ day_8.py
@@ -19,25 +19,36 @@ for ant in antennae:
     print(f'{ant} coords: {coords}')
 
 
-
-def antinode_coords(coords):
-    for i in range(len(coords)-1):
-        # section off ref antenna
-        antenna_1 = coords.pop(0)
-        # ref antenna coords
-        for a1_x,a1_y in antenna_1:
-            # comparison antenna coords
-            for a2_x,a2_y in coords:
-                # calc dist between ref antenna and comparision antenna
-                x_dist = max([a1_x, a2_x]) - min([a1_x], [a2_x])
-                y_dist = max([a1_y, a2_y]) - min([a1_y], [a2_y])
-                antinode_1 = 
-
-
-antinode_coords(coords)
-
 def antenna_distance(ant_1, ant_2):
+    dist = [
+        max(ant_1[0], ant_2[0]) - min(ant_1[0], ant_1[0]),
+        max(ant_1[1],ant_2[1]) - min(ant_1[1], ant_2[1])
+    ]
+    return dist    
+
+
+def antinode_position(antenna_1, antenna_2, dist):
+    antinode_1 = []
+    antinode_2 = []
+
+    if antenna_1[0] > antenna_2[0]:
+        antinode_1.append(antenna_1[0] + dist[0])
+        antinode_2.append(antenna_2[0] - dist[0])
+    else:
+        antinode_1.append(antenna_1[0] - dist[0])
+        antinode_2.append(antenna_2[0] + dist[0])
     
+    if antenna_1[1] > antenna_2[1]:
+        antinode_1.append(antenna_1[1] + dist[1])
+        antinode_2.append(antenna_2[1] - dist[1])
+    else:
+        antinode_1.append(antenna_1[1] - dist[1])
+        antinode_2.append(antenna_2[1] + dist[1])
+    
+    return antinode_1, antinode_2
+
+
+
 
 #TODO: iterate through group cooords, return antinode coords, save to new list
 #TODO: remove out-of-bounds antinode coords.
