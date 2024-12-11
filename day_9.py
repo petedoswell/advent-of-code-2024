3c3a838138fa16ec6087257956d75f12dc3b525d
Pete Doswell <pete.doswell@gmail.com>
2024-12-11 07:15:41 +0000

    solution path using reverse method, yields incorrect result

--- day_9.py
+++ day_9.py
@@ -1,9 +1,23 @@
-with open('day9_example.txt', 'r') as file:
-    data = list(file.read())
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='defrag_puzzle.log',
+    filemode='w'
+)
+
+with open('day9_input.txt', 'r') as file:
+    data = list(file.read().strip())
+
+
+# handle odd_numbered input
+if len(data)%2 != 0:
+    spare = int(data.pop())
 
 # split data into files and free_space
 files = [int(data[i]) for i in range(0,len(data),2)]
 free_space = [int(data[n]) for n in range(1,len(data),2)]
+
 
 #create tuples of files and free space
 disk_map = list(zip(files, free_space))
@@ -13,7 +27,7 @@ expanded_disk_map = []
 
 # get ID number of files and free space tuples
 for id, value in enumerate(disk_map):
-    print(id, value)
+
     # expand ID and number of files
     for i in range(value[0]):
         expanded_disk_map.append(id)
@@ -21,25 +35,34 @@ for id, value in enumerate(disk_map):
     for i in range(value[1]):
         expanded_disk_map.append('.')
 
-print(expanded_disk_map)
+final_number = [expanded_disk_map[-1] for x in range(spare)]
+for z in final_number:
+    expanded_disk_map.append(z)
 
-# reversed copy of disk_map to pull files from
-rev_disk_map = expanded_disk_map.copy()
-rev_disk_map.reverse()
+disk_size = len(expanded_disk_map)
 
-print(rev_disk_map)
+for i in range(disk_size):
+    if expanded_disk_map[i] == '.':
+        expanded_disk_map.reverse()
+        for j in range(disk_size):
+            if expanded_disk_map[j] != '.':
+                holding = expanded_disk_map[j]
+                expanded_disk_map[j] = '.'
+                expanded_disk_map.reverse()
 
-# iterate over expanded_disk_map, replpacing '.'s with files from end of rev_copy
-for i in range(len(expanded_disk_map)):
-	if expanded_disk_map[i] == '.':
-		for n in range(len(rev_disk_map)):
-			if rev_disk_map[n] != '.':
-				expanded_disk_map[i] = rev_disk_map[n]
-                # replace moved file with '.' so is skipped in next iteration
-				rev_disk_map[n] = '.'
-				break
+                break
+        expanded_disk_map[i] = holding
 
-print(expanded_disk_map)
+for i in range(disk_size):
+    if expanded_disk_map[i] == '.':
+        holding = expanded_disk_map[-1]
+        expanded_disk_map[i] = holding
+        expanded_disk_map[-1] = '.'
+        break
 
-#TODO: need to remove the moved files from end of expanded_disk_map to avoid dupes
-#TODO: do I need a 3rd list to assemble the defragged disk_map to avoid indexing errors & dupes?
+logging.info(expanded_disk_map)
+if len(expanded_disk_map) != len(data):
+    logging.debug(f'uneven sizes: \ndiskmap = {len(expanded_disk_map)}\noriginal data = {len(data)}')
+checksum = sum([index * value for index, value in enumerate(expanded_disk_map) if value != '.'])
+print(checksum)
+
